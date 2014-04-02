193d56b8c02d4228e9871812298a55448d6f7a7b
Omid Raha <omidraha.com@gmail.com>
2014-04-02 21:21:41 +0430

    Add absolute pass of template to `balot.TEMPLATE_PATH`
    Add absolute template path to the default balot `TEMPLATE_PATH`, 
    For cases where run `index.py` from elsewhere like: `$ python ~/mongoguestbook/index.py`
    and not from root of project like:`mongoguestbook$ python index.py`

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import os
 import bottle
 import pymongo
 import guestbookDAO
@@ -27,6 +28,15 @@ connection = pymongo.MongoClient(connection_string)
 database = connection.names
 #Finally, let out data access object class we built which acts as our data layer know about this
 guestbook = guestbookDAO.GuestbookDAO(database)
+
+# Getting base directory of project
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+# Gettting absolute path of templte
+template_path = os.path.join(BASE_DIR, 'views')
+# Add absolute template path to the default balot `TEMPLATE_PATH`, 
+# for when run `index.py` from elsewhere like: `python ~/mongoguestbook/index.py`
+# and not from root of project like: `mongoguestbook$ python index.py`
+bottle.TEMPLATE_PATH.insert(0, template_path)
 
 bottle.debug(True)
 bottle.run(host='localhost', port=8082) 
